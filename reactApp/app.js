4985d4c524ac8909e641d85a0a71e9e2cf45a297
hqiao97 <hqiao@wellesley.edu>
2017-07-19 16:35:30 -0700

    part 2

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,6 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+let dummyData = [
+  {taskText: 'Wash dishes', completed: true},
+  {taskText: 'go shopping', completed: false},
+  {taskText: 'profess my love to ricky', completed: false},
+  {taskText: 'attempt to kill myself', completed: false},
 
-ReactDOM.render(<p>React lives!</p>,
-   document.getElementById('root'));
+];
+
+class InputLine extends React.Component{
+  render() {
+    return (
+      <div>
+        <form>
+          <input type='text' placeholder='task' />
+          <input type='submit' value='Add todo'/>
+        </form>
+      </div>
+    )
+  }
+}
+
+
+class Todo extends React.Component{
+  render() {
+    return (
+      this.props.completed ? <li><button value="X">X</button><strike>{this.props.text}</strike></li> :
+                             <li><button value="X">X</button>{this.props.text}</li>
+    )
+  }
+}
+
+class TodoList extends React.Component{
+  render() {
+    return (
+      <ul>
+        {this.props.task.map((item) => <Todo key={item.taskText} text={item.taskText} completed={item.completed} />)}
+      </ul>
+    )
+  }
+}
+
+class TodoApp extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      todos: []
+    }
+  }
+
+  componentDidMount(){
+    this.setState({todos: dummyData})
+  }
+
+  render() {
+    return (
+      <div>
+        <InputLine />
+        <TodoList task={this.state.todos} />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<TodoApp />, document.getElementById('root'));
